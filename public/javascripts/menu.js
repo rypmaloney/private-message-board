e802ef2e292124675fde673810b44699bcc9cd39
Ryan Maloney <rypmaloney@gmail.com>
2022-02-26 12:12:47 -0500

    Improved style of the menu. Initial menu open js

---
+++ public/javascripts/menu.js
@@ -0,0 +1,30 @@
+function menuController() {
+    //Close menu when clicking away from it
+    // (function () {
+    //     window.addEventListener("mouseover", clickAway);
+    //     function clickAway(e) {
+    //         if (e.target.matches(".main")) {
+    //             let dropdown = document.getElementsByClassName("menu-item");
+    //             for (let i = 0; i < dropdown.length; i++) {
+    //                 if (dropdown[i].classList.contains("showMobile")) {
+    //                     dropdown[i].classList.remove("showMobile");
+    //                 }
+    //             }
+    //         }
+    //     }
+    // })();
+
+    //Mobile menu expand
+    (function () {
+        let menuBtn = document.getElementById("menu-opener");
+        menuBtn.addEventListener("click", () => expandMenu());
+        function expandMenu() {
+            let menuItem = document.getElementById("side-menu");
+            menuItem.classList.toggle("open");
+        }
+    })();
+
+    console.log("working");
+}
+
+menuController();
